d4579221ad5c8e611ece3cb0ce442f6c38868e41
CorerDuNord <matgalebg@gmail.com>
2026-05-19 08:56:18 +0200

    fix: use App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "sans-serif" }}>
+      <h1>Webhook Demo</h1>
+      <p>App déployée automatiquement via GitHub Webhook + ngrok in english</p>
+    </div>
+  );
+}
+
+export default App;
