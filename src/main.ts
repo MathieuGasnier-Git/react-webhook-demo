fd9858ccbf4bc193a26c04f8f98e14e6d82f8e7b
CorerDuNord <matgalebg@gmail.com>
2026-05-19 09:00:24 +0200

    chore: full english, no comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div style="text-align: center; margin-top: 100px; font-family: sans-serif;">
     <h1>Webhook Demo</h1>
-    <p>App déployée automatiquement via GitHub Webhook + ngrok in english</p>
+    <p>App automatically deployed via GitHub Webhook and ngrok</p>
   </div>
 `
