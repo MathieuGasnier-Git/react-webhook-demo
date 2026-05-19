9f38dd7279a513d1df721de3ead452b980ceaa8a
CorerDuNord <matgalebg@gmail.com>
2026-05-19 08:50:43 +0200

    test webhook

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "sans-serif" }}>
       <h1>Webhook Demo</h1>
-      <p>App déployée automatiquement via GitHub Webhook + ngrok</p>
+      <p>App déployée automatiquement via GitHub Webhook + ngrok in english</p>
     </div>
   );
 }
